957ec3983150a4de348034fbfb1ab9611ad8a2a8
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-03-02 14:15:37 -0300

    iKCAL: some additions
    * Added support for another kcal directory, i noticed that devices can contain different directories.
    
    * Small change in exit status in 'system/bin/ikcal'.
    
    Signed-off-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- common/install.sh
+++ common/install.sh
@@ -3,10 +3,22 @@
 # If you use parts of this project, please credit the respective authors.
 
 # KCAL working directory
-KCALDIR="/sys/devices/platform/kcal_ctrl.0"
+# Check if the kcal directory exists on the default path
+if [[ -d "/sys/devices/platform/kcal_ctrl.0" ]]; then
+    KCAL_DIR="/sys/devices/platform/kcal_ctrl.0"
+    KCAL_TDIR="0"
+# Check if the kcal directory exists on the alternate path
+elif [[ -d "/sys/module/msm_drm/parameters" ]]; then
+    KCAL_DIR="/sys/module/msm_drm/parameters"
+    KCAL_TDIR="1"
+else
+    true
+fi
 
 # Variable to know if kcal is active or deactivated
-KCALSTATUS="$(cat $KCALDIR/kcal_enable)"
+if [[ "$KCAL_TDIR" == "0" ]]; then
+  KCALSTATUS="$(cat $KCAL_DIR/kcal_enable)"
+fi
 
 # Modpath
 modpath="/data/adb/modules_update/iKCAL/"
@@ -14,7 +26,7 @@ modpath="/data/adb/modules_update/iKCAL/"
 # Function to store current kcal data
 backup_preset() {
   # Source and destination directories
-  src_dir="/sys/devices/platform/kcal_ctrl.0"
+  src_dir="$KCAL_DIR"
   dst_dir="/data/local/perm_kcal_backup"
 
   # Create the destination directory if it doesn't exist
@@ -23,12 +35,22 @@ backup_preset() {
   fi
 
   # Copy the files from the source to the destination directory
-  cp -r "$src_dir"/kcal "$dst_dir"
-  cp -r "$src_dir"/kcal_min "$dst_dir"
-  cp -r "$src_dir"/kcal_sat "$dst_dir"
-  cp -r "$src_dir"/kcal_hue "$dst_dir"
-  cp -r "$src_dir"/kcal_val "$dst_dir"
-  cp -r "$src_dir"/kcal_cont "$dst_dir"
+  if [[ "$KCAL_TDIR" == "0" ]]; then
+    cp -r "$src_dir"/kcal "$dst_dir"
+    cp -r "$src_dir"/kcal_min "$dst_dir"
+    cp -r "$src_dir"/kcal_sat "$dst_dir"
+    cp -r "$src_dir"/kcal_hue "$dst_dir"
+    cp -r "$src_dir"/kcal_val "$dst_dir"
+    cp -r "$src_dir"/kcal_cont "$dst_dir"
+  elif [[ "$KCAL_TDIR" == "1" ]]; then
+    cp -r "$src_dir"/kcal_red "$dst_dir"
+    cp -r "$src_dir"/kcal_green "$dst_dir"
+    cp -r "$src_dir"/kcal_blue "$dst_dir"
+    cp -r "$src_dir"/kcal_sat "$dst_dir"
+    cp -r "$src_dir"/kcal_hue "$dst_dir"
+    cp -r "$src_dir"/kcal_val "$dst_dir"
+    cp -r "$src_dir"/kcal_cont "$dst_dir"
+  fi
 
   # Message that signals the end of the backup
   sleep 1.5
@@ -49,27 +71,27 @@ ui_print ""
 ui_print "With this module you can choose handpicked kcal presets."
 sleep 1
 ui_print ""
-ui_print "[*] - Checking if your kernel supports kcal..."
+ui_print "[*] - Checking if your kernel supports KCAL..."
 sleep 1.5
 ui_print ""
 
 # Check if kcal is supported
-if [[ -d "$KCALDIR" ]]; then
-  ui_print "[*] - Your kernel supports kcal, proceeding with installation..."
+if [[ -d "$KCAL_DIR" ]]; then
+  ui_print "[*] - Your kernel supports KCAL, proceeding with installation..."
   sleep 1.5
-elif [[ ! -d "$KCALDIR" ]]; then
-  ui_print "[*] - Your kernel unfortunately does not support kcal, terminating installation..."
+elif [[ ! -d "$KCAL_DIR" ]]; then
+  ui_print "[*] - Your kernel unfortunately does not support KCAL, terminating installation..."
   sleep 1.5
   exit 1
 else
-  ui_print "[*] - Kcal support status is unknown, terminating installation..."
+  ui_print "[*] - KCAL support status is unknown, terminating installation..."
   sleep 1.5
   exit 1
 fi
 
 # Check if kcal is disabled and enable it
 if [[ "$KCALSTATUS" == "1" ]]; then
-  echo '0' > $KCALDIR/kcal_enable
+  echo '0' > $KCAL_DIR/kcal_enable
 fi
 
 # Kcal data backup
